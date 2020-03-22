1d30bfa4ac62dfd3c98361be0590d7a584cc84c0
Nathan <nathan.seva@outlook.fr>
2020-03-22 13:42:28 +0100

    Refactor projetc: make it runable

---
+++ projet_math_winform/Form1.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace projet_math_winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxc = new System.Windows.Forms.TextBox();
+            this.m = new System.Windows.Forms.Label();
+            this.textBoxm = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxdt = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxtf = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listViewE = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Equation :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "d(V(t))/dt = -cV²/m + p/m ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Variables :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "c";
+            // 
+            // textBoxc
+            // 
+            this.textBoxc.Enabled = false;
+            this.textBoxc.Location = new System.Drawing.Point(90, 38);
+            this.textBoxc.Name = "textBoxc";
+            this.textBoxc.Size = new System.Drawing.Size(100, 20);
+            this.textBoxc.TabIndex = 8;
+            this.textBoxc.Text = "-0,0000264";
+            // 
+            // m
+            // 
+            this.m.AutoSize = true;
+            this.m.Location = new System.Drawing.Point(36, 73);
+            this.m.Name = "m";
+            this.m.Size = new System.Drawing.Size(15, 13);
+            this.m.TabIndex = 9;
+            this.m.Text = "m";
+            // 
+            // textBoxm
+            // 
+            this.textBoxm.Location = new System.Drawing.Point(90, 70);
+            this.textBoxm.Name = "textBoxm";
+            this.textBoxm.Size = new System.Drawing.Size(100, 20);
+            this.textBoxm.TabIndex = 10;
+            this.textBoxm.Text = "0,01";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(454, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Conditions initiales";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(594, 30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "V(t=0) = 0 m/s";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Calculer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(220, 36);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Pas (dt)";
+            // 
+            // textBoxdt
+            // 
+            this.textBoxdt.Location = new System.Drawing.Point(277, 36);
+            this.textBoxdt.Name = "textBoxdt";
+            this.textBoxdt.Size = new System.Drawing.Size(100, 20);
+            this.textBoxdt.TabIndex = 17;
+            this.textBoxdt.Text = "0.01";
+            this.textBoxdt.TextChanged += new System.EventHandler(this.textBoxdt_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(223, 73);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "t(final)";
+            // 
+            // textBoxtf
+            // 
+            this.textBoxtf.Location = new System.Drawing.Point(277, 73);
+            this.textBoxtf.Name = "textBoxtf";
+            this.textBoxtf.Size = new System.Drawing.Size(100, 20);
+            this.textBoxtf.TabIndex = 19;
+            this.textBoxtf.Text = "5";
+            this.textBoxtf.TextChanged += new System.EventHandler(this.textBoxtf_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(155, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Euler";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(537, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "RK2";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 63);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(867, 499);
+            this.tabControl1.TabIndex = 23;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listViewE);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(859, 473);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tableau de valeurs";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // listViewE
+            // 
+            this.listViewE.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listViewE.Location = new System.Drawing.Point(15, 24);
+            this.listViewE.Name = "listViewE";
+            this.listViewE.Size = new System.Drawing.Size(827, 443);
+            this.listViewE.TabIndex = 23;
+            this.listViewE.UseCompatibleStateImageBehavior = false;
+            this.listViewE.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Temps t(s)";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Vitesse V (m/s)";
+            this.columnHeader2.Width = 136;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Position x (m)";
+            this.columnHeader3.Width = 161;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Force de poussée p (N)";
+            this.columnHeader4.Width = 129;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Vitesse V (m/s)";
+            this.columnHeader5.Width = 152;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Position x (m)";
+            this.columnHeader6.Width = 166;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.label12);
+            this.tabPage2.Controls.Add(this.label11);
+            this.tabPage2.Controls.Add(this.textBoxm);
+            this.tabPage2.Controls.Add(this.textBoxtf);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label10);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.textBoxc);
+            this.tabPage2.Controls.Add(this.textBoxdt);
+            this.tabPage2.Controls.Add(this.m);
+            this.tabPage2.Controls.Add(this.label9);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(859, 473);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Constantes";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(166, 135);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 24;
+            this.textBox2.Text = "560";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 23;
+            this.textBox1.Text = "0.0005";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(39, 180);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(101, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Constante de temps";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(39, 135);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(80, 13);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Poussée initiale";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 567);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Projet Maths";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxc;
+        private System.Windows.Forms.Label m;
+        private System.Windows.Forms.TextBox textBoxm;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxdt;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxtf;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListView listViewE;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ projet_math_winform/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+//using NsExcel = Microsoft.Office.Interop.Excel;
+
+namespace projet_math_winform
+{
+    public partial class Form1 : Form
+    {
+        double m_n = 0.0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e){}
+
+        private void listViewE_SelectedIndexChanged(object sender, EventArgs e){}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calculate(400);
+        }
+
+        private void Form1_Load(object sender, EventArgs e){calculate_n();}
+
+        private void calculate_n()
+        {
+            if (double.TryParse(textBoxtf.Text.Replace('.', ','), out m_n) && double.TryParse(textBoxdt.Text.Replace('.', ','), out m_n))
+            {
+                m_n = (double.Parse(textBoxtf.Text.Replace('.', ',')) - 0) / double.Parse(textBoxdt.Text.Replace('.', ','));
+            }
+        }
+
+        private void textBoxdt_TextChanged(object sender, EventArgs e)
+        {calculate_n();}
+
+        private void textBoxtf_TextChanged(object sender, EventArgs e)
+        {calculate_n();}
+
+        private List<double> calculate(double Pmax)
+        {
+            listViewE.Items.Clear();  // clear listview
+            double t0 = 0.0;
+            double tf = double.Parse(textBoxtf.Text.Replace('.', ','));
+            double c = double.Parse(textBoxc.Text.Replace('.', ','));
+            double m = double.Parse(textBoxm.Text.Replace('.', ','));  // 0.001
+            double dt = double.Parse(textBoxdt.Text.Replace('.', ','));
+            double To = double.Parse(textBox1.Text.Replace('.', ','));
+            double P0 = double.Parse(textBox2.Text.Replace('.', ','));
+
+            double v;  // v (i-1)
+            double vv;  // v (i)
+            double vv2;  // v (i) RK2
+            double k1;  // K1 RK2
+            double k2;  // K2 RK2
+            double v1;  // V(i) + K1  // RK2
+            double t;
+            double x;  // x (i-1)
+            double xx = 0.0; // x (i)
+            double xx2 = 0.0; // x (i)  RK2
+            double p = P0;
+            double v0 = 0;
+            List<double> tabV = new List<double> { v0 };
+            List<double> tabV2 = new List<double> { v0 };  // RK2
+            double x0 = 0.0;
+            List<double> tabx = new List<double> { x0, x0 };
+            List<double> tabx2 = new List<double> { x0, x0 };  // RK2
+
+            ListViewItem item = new ListViewItem(new string[] { t0.ToString(), v0.ToString(), "0", p.ToString(), v0.ToString(), "0" });
+            listViewE.Items.Add(item);
+            for (int i = 1; i <= m_n; i++)
+            {
+                v = tabV[i - 1];
+                t = t0 + i * dt;
+                if (i > 1) p = 0;
+
+                // Euler
+                // V :
+                vv = v + dt * ((c * v * v / m) + (p / m));
+                tabV.Add(vv);
+
+                // x :
+                if (i > 1)
+                {
+                    x = tabx[i - 1];
+                    xx = x + dt * vv;
+                    tabx.Add(xx);
+                }
+
+                // RK2
+                // V :
+                v = tabV2[i - 1];
+                k1 = dt * ((c * v * v / m) + (p / m));
+                v1 = v + k1;
+                k2 = dt * ((c * v1 * v1 / m) + (p / m));
+
+                vv2 = v + (k1 + k2) / 2;
+                // x :
+                if (i > 1)  
+                {
+                    x = tabx[i - 1];
+                    xx2 = x + dt * vv2;
+                    tabx2.Add(xx2);
+                }
+                tabV2.Add(vv2);
+
+                // Affichage
+
+                item = new ListViewItem(new string[] { t.ToString(), vv.ToString(), xx.ToString(), p.ToString(), vv2.ToString(), xx2.ToString() });
+                listViewE.Items.Add(item);
+                export(tabV, "results_EULER.csv");
+                export(tabV2, "results_RK2.csv");
+                export(tabx, "results_X_EULER.csv");
+                export(tabx2, "results_X_RK2.csv");
+            }
+            return tabV2;
+        }
+
+        private void export(List<double> list, string filename)
+        {
+            using (StreamWriter sw = File.CreateText(filename))
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    sw.WriteLine(list[i]);
+                }
+            }
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+        /*
+public void ListToExcel(List<string> list)
+//  https://stackoverflow.com/questions/2206279/exporting-the-values-in-list-to-excel
+{
+   //start excel
+   NsExcel.ApplicationClass excapp = new Microsoft.Office.Interop.Excel.ApplicationClass();
+
+   //if you want to make excel visible           
+   excapp.Visible = true;
+
+   //create a blank workbook
+   var workbook = excapp.Workbooks.Add(NsExcel.XlWBATemplate.xlWBATWorksheet);
+
+   //or open one - this is no pleasant, but yue're probably interested in the first parameter
+   string workbookPath = "C:\test.xls";
+   var workbook = excapp.Workbooks.Open(workbookPath,
+       0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "",
+       true, false, 0, true, false, false);
+
+   //Not done yet. You have to work on a specific sheet - note the cast
+   //You may not have any sheets at all. Then you have to add one with NsExcel.Worksheet.Add()
+   var sheet = (NsExcel.Worksheet)workbook.Sheets[1]; //indexing starts from 1
+
+   //do something usefull: you select now an individual cell
+   var range = sheet.get_Range("A1", "A1");
+   range.Value2 = "test"; //Value2 is not a typo
+
+   //now the list
+   string cellName;
+   int counter = 1;
+   foreach (var item in list)
+   {
+       cellName = "A" + counter.ToString();
+       var range_ = sheet.get_Range(cellName, cellName);
+       range_.Value2 = item.ToString();
+       ++counter;
+   }
+
+   //you've probably got the point by now, so a detailed explanation about workbook.SaveAs and workbook.Close is not necessary
+   //important: if you did not make excel visible terminating your application will terminate excel as well - I tested it
+   //but if you did it - to be honest - I don't know how to close the main excel window - maybee somewhere around excapp.Windows or excapp.ActiveWindow
+}
+*/
+
+        // private void button2_Click(object sender, EventArgs e){}
+
+        /*private void button2_Click_1(object sender, EventArgs e)
+        {
+            List<double> list;
+            for(int i = 0; i < 10; i++)
+            {
+                list = calculate(i * 4);
+                if()
+            }
+        }*/
+    }
+}
